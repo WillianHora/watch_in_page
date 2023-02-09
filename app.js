c20271ddf0a2e1b1da6f457bf847fdef61b254ed
Willian Hora <99353071+WillianHora@users.noreply.github.com>
2023-02-09 17:13:55 -0300

    create_page

---
+++ app.js
@@ -0,0 +1,42 @@
+  const hours = document.getElementById("hours");
+  const minutes = document.getElementById("minutes");
+  const seconds = document.getElementById("seconds");
+
+  setInterval(function () {
+    var agora = new Date();
+    let hora = agora.getHours();
+    var minutos = agora.getMinutes()
+    var segundos = agora.getSeconds();
+    hours.innerHTML = hora;
+    minutes.innerHTML = minutos;
+    seconds.innerHTML = segundos;
+     if (minutos < 10) {
+       minutes.innerHTML = "0"+minutos;
+     }
+    
+ }, 1001);
+
+
+ var agora = new Date();
+ let hora = agora.getHours();
+
+ if(hora>18){
+    document.body.classList.toggle("dark");
+ }
+
+
+
+
+
+
+   
+
+
+
+
+
+ 
+
+ 
+  
+
