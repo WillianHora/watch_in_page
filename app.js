7718548237c3866e34f6f31654e3a1241d18df8a
Willian Hora <99353071+WillianHora@users.noreply.github.com>
2023-05-05 15:13:57 -0300

    add_afternoon

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
   const minutes = document.getElementById("minutes");
   const seconds = document.getElementById("seconds");
 
-  setInterval(function () {
-    var agora = new Date();
+   setInterval(function () {
+   var agora = new Date();
     let hora = agora.getHours();
     var minutos = agora.getMinutes()
     var segundos = agora.getSeconds();
     hours.innerHTML = hora;
     minutes.innerHTML = minutos;
     seconds.innerHTML = segundos;
+    
      if (minutos < 10) {
        minutes.innerHTML = "0"+minutos;
        }
@@ -20,11 +21,16 @@
  }, 1001);
 
 
- var agora = new Date();
+ var agora = new Date(); 
  let hora = agora.getHours();
 
- if(hora>=18){
-    document.body.classList.toggle("dark");
+
+ if(hora>=4 && hora<=15){
+    document.body.classList.toggle("light");
+ } else if (hora>=16 && hora<18){
+  document.body.classList.toggle("mid");
+ }else if (hora>=18 || hora<=3){
+  document.body.classList.toggle("dark");
  }
 
 
